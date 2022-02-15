b9cedac201e8a407d279369d0117059095449462
KaiqueSantos21 <kaiqueosantos@gmail.com>
2022-02-15 17:18:32 -0300

    Distribuição e instruções de execução

--- src/main/java/kaiqueosantos/desafio_de_programacao/App.java
+++ src/main/java/kaiqueosantos/desafio_de_programacao/App.java
@@ -34,6 +34,10 @@ public class App
 			int quantidadeParesAnagramas = Questoes.questao3(Palavra);
 			
 			System.out.println(quantidadeParesAnagramas + " pares de anagramas foram encontrados.");
+			
+			System.out.println();
+			System.out.println("Pressione Enter para encerrar a aplicacao.");
+			sc.nextLine();
 		}
 		catch (InputMismatchException exception) {
 			System.out.println("Erro inesperado: entrada invalida.");
